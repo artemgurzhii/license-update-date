e9fddc538185fd847385b0bc5650288a18624357
Artem Gurzhii <artem.gurzhii@gmail.com>
2019-10-20 21:28:33 +0200

    add babel & eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,23 @@
 module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+
   parserOptions: {
     ecmaVersion: 2018,
+    sourceType: 'module',
   },
 
-  plugins: ['prettier', 'node'],
+  plugins: ['prettier', 'babel', 'node'],
   extends: ['eslint:recommended', 'plugin:prettier/recommended', 'plugin:node/recommended'],
+
   env: {
     node: true,
   },
-  rules: {},
-  overrides: [
-    {
-      files: ['__tests__/**/*.js'],
-      env: {
-        jest: true,
-      },
-    },
-  ],
+
+  rules: {
+    'node/no-unsupported-features/es-syntax': 0,
+    'node/no-unpublished-import': 0,
+  },
+
+  overrides: [],
 };
